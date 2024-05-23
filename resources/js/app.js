3ed4173d16087c1f21ef0f42d0b8a381d5cade1c
WidyaX1900 <tuderangga93@gmail,com>
2024-05-23 13:27:42 +0800

    Import bootstrap css files to app.js file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,4 @@
 import "./bootstrap";
 import "../css/app.css";
 import "../css/utility.css";
+import "../bootstrap/css/bootstrap.min.css";
--- resources/views/templates/layout.blade.php
+++ resources/views/templates/layout.blade.php
@@ -6,16 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>AyoBaca - Book Online Store</title>
-    <link rel="stylesheet" href="{{ config('app.asset_url') }}bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
-    @vite('resources/js/app.js')
+    @vite(['resources/js/app.js', 'resources/jquery/jquery-3.7.1.js'])
 </head>
 
 <body>
     @yield('navbar')
     @yield('content')
     @yield('footer')
-    <script src="{{ config('app.asset_url') }}js/jquery-3.7.1.js"></script>
+    @vite('resources/js/script.js')
 </body>
 
 </html>
