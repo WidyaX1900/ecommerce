9f82cc828fd83df085d27a0ddcd61316c88b7220
WidyaX1900 <tuderangga93@gmail,com>
2024-05-23 11:38:21 +0800

    Import css files to app.js file

--- resources/views/templates/layout.blade.php
+++ resources/views/templates/layout.blade.php
@@ -8,7 +8,7 @@
     <title>Ayo Baca - Book Online Store</title>
     <link rel="stylesheet" href="{{ config('app.asset_url') }}bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
-    @vite(['resources/css/app.css', 'resources/css/utility.css'])
+    @vite('resources/js/app.js')
 </head>
 
 <body>
@@ -18,7 +18,6 @@
         Copyright &copy; 2023 by <strong class="ms-1">Rangga Widya</strong>
     </footer>
     <script src="{{ config('app.asset_url') }}js/jquery-3.7.1.js"></script>
-    @vite('resources/js/app.js')
 </body>
 
 </html>
